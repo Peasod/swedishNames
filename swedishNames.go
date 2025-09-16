2f89ceb948427c395039ba1115430d67cf2a8c06
Peter Söderström <psoderstrom.pro@outlook.com>
2025-09-16 11:26:25 +0200

    Secondary function test.

--- swedishNames.go
+++ swedishNames.go
@@ -5,3 +5,9 @@ import "fmt"
 func LibTest() {
 	fmt.Println("Testing import statement from swedishNames package.")
 }
+
+// A description, just a test.
+func function1() {
+	fmt.Println("Testing import statement from swedishNames package. This is from the function 'function1'.")
+
+}

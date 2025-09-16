122c17949f7be87405b4151845c274e8588417ee
Peter Söderström <psoderstrom.pro@outlook.com>
2025-09-16 11:42:58 +0200

    Updated export funtions.

--- swedishNames.go
+++ swedishNames.go
@@ -7,7 +7,13 @@ func LibTest() {
 }
 
 // A description, just a test.
-func function1() {
-	fmt.Println("Testing import statement from swedishNames package. This is from the function 'function1'.")
+func Function1() {
+	fmt.Println("Testing import statement from swedishNames package. This is from the function 'Function1'.")
+
+}
+
+// A description, just a another export test.
+func Function2() {
+	fmt.Println("Testing import statement from swedishNames package. This is from the function 'Function2'.")
 
 }

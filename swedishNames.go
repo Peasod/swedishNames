181af19b526645067f8771435244af1b6295b094
Peter Söderström <psoderstrom.pro@outlook.com>
2025-09-22 21:55:42 +0200

    GetFnamn(), returnerar en sträng.

--- swedishNames.go
+++ swedishNames.go
@@ -1,19 +1,15 @@
 package swedishNames
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+)
 
-func LibTest() {
-	fmt.Println("Testing import statement from swedishNames package.")
-}
+// GetFnamn fetches data from a specific SCB API endpoint and returns the response body as a string.
+func GetFnamn() string {
+	resp, _ := http.Get("https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0001/BE0001G/BE0001T06AR")
+	defer resp.Body.Close()
+	x, _ := io.ReadAll(resp.Body)
 
-// A description, just a test.
-func Function1() {
-	fmt.Println("Testing import statement from swedishNames package. This is from the function 'Function1'.")
-
-}
-
-// A description, just a another export test.
-func Function2() {
-	fmt.Println("Testing import statement from swedishNames package. This is from the function 'Function2'.")
-
+	return string(x)
 }
